a8b20ea947d44206f837f90a6f9f26105288e7e8
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2024-01-15 09:11:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+	.then(resopnse => {
+		if(!resopnse.ok){
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+		return resopnse.json();
+	})
+	.then(data => {
+		console.log('Response:');
+        console.log('User ID:', data.userId);
+        console.log('ID:', data.id);
+        console.log('Title:', data.title);
+        console.log('Completed:', data.completed);
+	})
+	.catch(err => {
+		console.error('Error:', error.message);
+	})
